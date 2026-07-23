655a3c06cf23c4924f0de7ccd5e4cd456cf7899b
Danny <d.chahbour@zeroandone.me>
2026-07-22 18:08:18 -0700

    Add function to all menu items

--- Interface.py
+++ Interface.py
@@ -1,3 +1,4 @@
+import datetime
 def menu():
      print ("1) Add appointment")
      print ("2) List appointment")
@@ -5,27 +6,75 @@ def menu():
      print ("4) Exit")
 
 def main():
+     app_info = []
+     appID = 0
      while True:
           menu()
-          user_info = {}
+          
           choice = input("Enter a number from 1-4 to select a feature: ").strip()
 
-          # Work in progress, choices should have function with a dict
+          # Add a dictionary to a list with the relevant information
           if choice == "1":
-               print("Add appointment function here\n")
-               name = input("What is your name? ").strip()
+               user_info = {}
+               appID += 1
+               
+               while True: # Ensures that the name field cannot be empty
+                    name = input("What is your name? ").strip()
+                    if name:
+                         break
+                    print("Name cannot be empty. Please enter your name.")
+
                service = input("What service would you like? ").strip()
-               date = input("What date would you like the appointment to be on? ")
-               time = input("What time of day would like the appointment to be at? ")
+
+               while True:
+                    date = input("What date would you like the appointment to be on? Please use YYYY-MM-DD format. ").strip()
+
+                    try:
+                         datetime.strptime(date, "%Y-%m-%d")
+                         break  # Valid date
+                    except ValueError:
+                         print("Invalid date. Please use YYYY-MM-DD (example: 2026-08-31).")
+
+               time = input("What time of day would like the appointment to be at? ").strip()
+
+               appKey = "Appointment ID"
+               nameKey = "Name"
+               serviceKey = "Service"
+               dateKey = "Date"
+               timeKey = "Time"
+
+               user_info[appKey] = appID
+               user_info[nameKey] = name
+               user_info[serviceKey] = service
+               user_info[dateKey] = date
+               user_info[timeKey] = time
+
+               app_info.append(user_info)
+               print()
+               print(user_info)
+               print()
 
           elif choice == "2":
-               print("Add list appointment function here\n")
+               if not app_info:
+                    print("\nThere are no appointments to display.\n")
+               else:
+                    print(f"\n{app_info}\n")
+               
           
           elif choice == "3":
-               print("Add cancel appointment function here\n")
+
+               while True: #Loops request until a valid input is inputted
+                    cancelID = int(input("Enter the appointment ID to cancel: "))
+
+                    if any(user["Appointment ID"] == cancelID for user in app_info): # Checks for user inputted appointment ID to ensure it exists before attempting to delete it
+                         app_info = [user for user in app_info if user["Appointment ID"] != cancelID]
+                         print(f"Appointment {cancelID} removed.")
+                         break
+                    else:
+                         print("That appointment ID does not exist.")
 
           elif choice == "4":
-               print("Add exit function here\n")
+               print("\nClosing program.\n")
                break
           
           else: # Prints an error in case of incorrect input
